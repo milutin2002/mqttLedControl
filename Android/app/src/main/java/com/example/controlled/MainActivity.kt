3d3acf97a5a65c0f37c8082afdc312356c3bef5f
milutin <milutin.jovanovic@elfak.rs>
2025-09-30 01:10:26 +0200

    Started android app

---
+++ Android/app/src/main/java/com/example/controlled/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.controlled
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.controlled.ui.theme.ControlLedTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ControlLedTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun LedControllScreen(){
+    val context= LocalContext.current
+    val scope= rememberCoroutineScope()
+
+    val broker =remember {   "tcp://broker.emqx.io:1883"}
+
+
+    var lastLedStatus by remember{ mutableStateOf<String?>(null) }
+
+
+    val topicSet= remember { "pico/led/set" }
+    val topicStatus =remember { "pico/led/status" }
+    fun connect(){
+        
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ControlLedTheme {
+        Greeting("Android")
+    }
+}
