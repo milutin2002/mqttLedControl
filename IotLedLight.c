f194b7b7fed883849e5dedb1e91cc44fd01fc41e
milutin <milutinjovanovic2002@gmail.com>
2025-10-06 00:26:04 +0200

    Added wifi, not yet finished wifi part

--- IotLedLight.c
+++ IotLedLight.c
@@ -1,22 +1,57 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
 #include "FreeRTOS.h"
 #include  "task.h"
 #include "queue.h"
 
 #define LED_PIN 13
 
+#ifndef WIFI_SSID
+#define WIFI_SSID "Change me"
+#endif
+
+#ifndef WIFI_PASS
+#define WIFI_PASS "Change me"
+#endif
+
 typedef enum { LED_OFF_MSG,LED_ON_MSG}msg_t;
 
 static QueueHandle_t queueHandle;
+
+void wifiTask(void * param){
+    if(cyw43_arch_init()){
+        printf("Wifi task problem\n");
+        while(true){
+            vTaskDelay(pdMS_TO_TICKS(1000));
+        }
+    }
+    cyw43_arch_enable_sta_mode();
+    printf("Connecting to wifi ssid\n",WIFI_SSID);
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    int rc;
+    while((rc=cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID,WIFI_PASS,CYW43_AUTH_WPA2_AES_PSK,20000))!=0){
+        printf("Retrying wifi passwords with ");
+        printf(WIFI_SSID);
+        printf(" ");
+        printf(WIFI_PASS);
+        printf("\n");
+        printf("Some other error happened with code %d\n",rc);
+        vTaskDelay(pdMS_TO_TICKS(3000));  
+    }
+    while(true){
+        printf("Wifi connected\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
 
 void blinkTask(void *param){
     bool ledState=false;
     while(true){
         ledState=!ledState;
         gpio_put(LED_PIN,ledState);
-        msg_t msg= ledState?LED_OFF_MSG:LED_ON_MSG;
-        xQueueSend(queueHandle,&msg,0);
+        //msg_t msg= ledState?LED_OFF_MSG:LED_ON_MSG;
+        //xQueueSend(queueHandle,&msg,0);
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
@@ -39,8 +74,9 @@ int main()
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN,GPIO_OUT);
     queueHandle=xQueueCreate(4,sizeof(msg_t));
+    xTaskCreate(wifiTask,"Wifi task",256,NULL,tskIDLE_PRIORITY+1,NULL);
     xTaskCreate(blinkTask,"Blink task",256,NULL,tskIDLE_PRIORITY+1,NULL);
-    xTaskCreate(loggerTask,"Logger task",256,NULL,tskIDLE_PRIORITY+1,NULL);
+    //xTaskCreate(loggerTask,"Logger task",256,NULL,tskIDLE_PRIORITY+1,NULL);
     vTaskStartScheduler();
     while(true){}
 }
---
+++ lwipopts.h
@@ -0,0 +1,52 @@
+// lwipopts.h – minimal config for Pico W + FreeRTOS + sockets + MQTT
+
+#ifndef LWIPOPTS_H
+#define LWIPOPTS_H
+
+// ----- OS integration -----
+#define NO_SYS                 0
+#define SYS_LIGHTWEIGHT_PROT   1
+
+// ----- Memory pools (tweak if you see OOM) -----
+#define MEM_ALIGNMENT          4
+#define MEM_SIZE               (50 * 1024)
+
+#define MEMP_NUM_TCP_PCB       8
+#define MEMP_NUM_UDP_PCB       4
+#define MEMP_NUM_TCP_SEG       32
+#define PBUF_POOL_SIZE         16
+#define PBUF_POOL_BUFSIZE      1536
+
+// ----- TCP/IP -----
+#define LWIP_TCP               1
+#define TCP_MSS                1460
+#define TCP_SND_BUF            (8 * TCP_MSS)
+#define TCP_WND                (8 * TCP_MSS)
+
+#define LWIP_UDP               1
+#define LWIP_ICMP              1
+#define LWIP_DNS               1
+#define LWIP_DHCP              1
+
+// ----- Sockets / Netconn (handy for tests) -----
+#define LWIP_SOCKET            1
+#define LWIP_NETCONN           1
+
+// ----- Checksum (use hardware/driver where possible) -----
+#define CHECKSUM_BY_HARDWARE   0
+
+// ----- MQTT app -----
+#define LWIP_ALTCP             0
+#define LWIP_ALTCP_TLS         0
+#define LWIP_MDNS_RESPONDER    0
+
+// ----- Thread settings (FreeRTOS) -----
+#define TCPIP_THREAD_STACKSIZE     2048
+#define DEFAULT_THREAD_STACKSIZE   1024
+#define TCPIP_MBOX_SIZE            16
+#define DEFAULT_RAW_RECVMBOX_SIZE  8
+#define DEFAULT_UDP_RECVMBOX_SIZE  8
+#define DEFAULT_TCP_RECVMBOX_SIZE  8
+#define DEFAULT_ACCEPTMBOX_SIZE    8
+
+#endif /* LWIPOPTS_H */
